8ed93216745983846e7e46c5b1a2c554c211d434
Mirazam2207 <85666322+Mirazam2207@users.noreply.github.com>
2021-08-17 16:50:46 +0500

    Add files via upload

---
+++ 3danimationapp/app.js
@@ -0,0 +1,11 @@
+var frog=document.getElementById("frog");
+
+function stop(){
+    frog.animationName='';
+}
+function run(){
+    frog.animationName='Running';
+}
+function wave(){
+    frog.animationName='Wave';
+}
